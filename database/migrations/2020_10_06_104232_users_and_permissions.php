ef8b58c6359a1098728cb50b01d6c9863bc879ca
pzar <piotr.zarzeka@softwebo.pl>
2020-10-07 09:27:17 +0200

    add users and their permissions

--- database/migrations/2020_10_06_104232_users_and_permissions.php
+++ database/migrations/2020_10_06_104232_users_and_permissions.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
 
 class UsersAndPermissions extends Migration
 {
